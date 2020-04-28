e4dbc5a8d6cdc65a483d77081925c0482380cfc0
jaime <jmfernandez1@miuandes.cl>
2020-04-28 05:37:21 -0400

    Laboratorio 5 listo

---
+++ MailSender.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Lab_5_Jaime_Fernandez
+{
+    public class MailSender
+    {
+
+        public delegate void SendEmail();
+
+        public event SendEmail EmailSent;
+
+        public void OnSend()
+        {
+            Console.WriteLine("Email Sent");
+        }
+
+        public void OnRegistered(object source, RegisterEventArgs e)
+        {
+            Thread.Sleep(2000);
+            Console.WriteLine($"\nCorreo enviado a {e.Email}: \n Gracias por registrarte, {e.Username}!\n Por favor, para poder verificar tu correo, has click en: {e.VerificationLink}\n");
+            Thread.Sleep(2000);
+        }
+
+        public void OnPasswordChanged(object source, ChangePasswordEventArgs e)
+        {
+            Thread.Sleep(2000);
+            Console.WriteLine($"\nCorreo enviado a {e.Email}:  \n {e.Username}, te notificamos que la contrasena de tu cuenta PlusCorp ha sido cambiada. \n");
+            Thread.Sleep(2000);
+        }
+
+    }
+}
---
+++ User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5_Jaime_Fernandez
+{
+    class User
+    {
+        public delegate void VerifyEmail();
+
+        public event VerifyEmail EmailVerified;
+
+        public void OnEmailSent()
+        {
+             Console.WriteLine("Do you want to verify your email?: Y/N");
+             string var = Console.ReadLine();
+             if (var == "Y")
+             {
+                Console.WriteLine("Verification email sent");
+             }
+        }
+    }
+}
